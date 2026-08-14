4479eead4ee79922590ff88db2e6fe006ffbed5e
forest-whispers <adityasingh240508@gmail.com>
2026-08-14 12:07:14 +0530

    added render url to frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: "https://teamhub-y3lj.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
       },
